55d4efc02b18a068b90b6a9616eba27de8c2d46a
yossefdahan <yossefdahan1@gmail.com>
2024-02-13 14:47:39 +0200

    add new width & height & innerText to the ball

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@
 
 
 function onBallClick() {
-
-    alert('hey')
+    var currHeight = 100
+    var currWidth = 100
+    var newHeight = currHeight + 50 + 'px'
+    var newWidth = currWidth + 50 + 'px'
+    var elBall = document.querySelector('div')
+    elBall.style.height = newHeight
+    elBall.style.width = newWidth
+    elBall.innerText = newHeight + ',' + newWidth
 }
