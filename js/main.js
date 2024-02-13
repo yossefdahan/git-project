43cac26a4ed520fd90a23fc039e490ac33d997bc
yossefdahan <yossefdahan1@gmail.com>
2024-02-13 16:41:59 +0200

    Use random function for ball size

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
 'use strict'
-var currHeight = 100
-var currWidth = 100
+var currSize = 100
 
 function onBallClick() {
     var elBall = document.querySelector('div')
+    currSize += getRandomInt(20, 60)
 
-    currHeight += 50
-    currWidth += 50
+    elBall.style.height = currSize + 'px'
+    elBall.style.width = currSize + 'px'
+    elBall.innerText = currSize
 
-    elBall.style.height = currHeight + 'px'
-    elBall.style.width = currWidth + 'px'
-    elBall.innerText = currHeight
-    if (currHeight >= 400) {
-        currHeight = 100
-        currWidth = 100
+    if (currSize >= 400) {
+        currSize = 100
     }
 }
